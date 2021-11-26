4fe5ad53d50384d8868bcd8d1e161cdc8bbf9c35
Brady Tran <baotrank22tpm4@gmail.com>
2021-11-26 12:48:01 +0700

    Practice MaterialUI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Typography, AppBar,Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core'
+import { PhotoCamera } from '@material-ui/icons';
+import useStyles from './styles';
 
+const cards = [1,2,3,4,5,6,7,8,9];
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position='relative'>
+        <Toolbar>
+          <PhotoCamera className={classes.icon}/>
+          <Typography variant='h6'>
+            Photo Album
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth="sm">
+            <Typography variant='h2' align='center' color='textPrimary' gutterBottom>
+              Photo Album
+            </Typography>
+            <Typography variant='h5' align='center' color='textSecondary' paragraph>
+              Hello everyone. This is a photo album and I'm trying to make this sentence as long as posible so we can see how does it work
+            </Typography>
+            <div className={classes.buttons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant='contained' color='primary'>
+                    See my Photo
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant='outlined' color='primary'>
+                    See my Photo 2
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {cards.map((card)=>
+            <Grid item key={card} xs={12} sm={6} md={4}>
+              <Card className={classes.card}>
+                <CardMedia className={classes.cardMedia} image="https://source.unsplash.com/random" title="image title"/>
+                <CardContent className={classes.CardContent}>
+                  <Typography gutterBottom variant='h5'>
+                    Heading
+                  </Typography>
+                  <Typography>
+                    This is a media card, abcd make this sentence as long as posible
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">View</Button>
+                  <Button size="small" color="primary">Edit</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            )}
+          </Grid>
+        </Container>
+      </main>
+      <footer className={classes.footer}>
+        <Typography variant="h6" align='center' gutterBottom>Footer</Typography>
+        <Typography variant="subtitle1" align='center' color='textSecondary'>This is footer and this is footer!</Typography>
+      </footer>
+    </>
   );
 }
 
